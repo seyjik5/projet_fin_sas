0ad7eed1fb8eee2dbd6f7ee02c24ea17ebe5f959
Tachfine Rachglou <seyjik5@gmail.com>
2026-09-24 18:02:40 +0100

    18:02

--- projet.js
+++ projet.js
@@ -1,1 +1,48 @@
-test hello 
+const prompt = require("prompt-sync")()
+
+
+menuPrincipal()
+
+
+let choix = prompt("Tapez un numero : ")
+
+switch (choix) {
+    case "1":
+        
+        break;
+    case "2" :
+
+        break;
+    case "3" :
+        break;
+    default:
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Fonctions : ----------------------------------------------------------------------------------
+
+function menuPrincipal(){
+    console.log("1 : Afficher candidat")
+    console.log("2 : Afficher la liste des candidats")
+    console.log("3 : Voter")
+    console.log("4 : Modifier les informations d'un candidat ")
+    console.log("5 : Supprimer un candidats")
+    console.log("6 : Rechercher des candidats")
+    console.log("7 : Statistiques de l'élection")
+}
+
+function choix(){
+
+}
